dc63003f4d8e3686c78bd271f6075caddc42f0f8
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-04-11 11:47:09 -0700

    handing in product

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,30 @@ import AppBar from "./components/AppBar";
 //styling and material-ui
 import "./App.css";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
 //JSShowdown converter variables
 import showdown from "showdown";
 const converter = new showdown.Converter();
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    height: "100vh",
+  },
+  area: {
+    fontSize: "1rem",
+    width: "100%",
+    height: "90vh",
+    outline: "none",
+  },
+}));
+
 function App() {
   //hook records the changes that I make
   const [myHtml, setMyHtml] = useState();
+  //set classes constant for material-ui
+  const classes = useStyles();
 
   const userInput = (event) => {
     const html = converter.makeHtml(event.target.value);
@@ -25,14 +41,19 @@ function App() {
   return (
     <div className="App">
       <AppBar />
-      <Grid container soacung={3}>
-        <Grid item xs={6}>
-          <textarea className="data__area" onChange={userInput}></textarea>
+      <div className={classes.root}>
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <textarea className={classes.area} onChange={userInput}></textarea>
+          </Grid>
+          <Grid item xs={6}>
+            <div
+              className={classes.area}
+              dangerouslySetInnerHTML={myHtml}
+            ></div>
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <div className="data__area" dangerouslySetInnerHTML={myHtml}></div>
-        </Grid>
-      </Grid>
+      </div>
     </div>
   );
 }
